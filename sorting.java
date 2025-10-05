34ab92e97368decc5a75d38252ef2abbbaa0b7f6
Bhavneet Verma <bhavneetv295@gmail.com>
2025-10-05 14:14:19 +0530

    sorting done

--- sorting.java
+++ sorting.java
@@ -9,11 +9,11 @@ public class sorting {
     public static void main(String[] args) {
 
         // bubble sorting
-
-        int arr[] = { 4, 5, 2, 7, 8, 1 };
+        // time complexity = O(n^2)
+        int arr[] = { 4, 5, 2, 7, 8, 1, 55 };
 
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -23,6 +23,29 @@ public class sorting {
             }
         }
         displayArray(arr);
+
+
+
+        // Selection Sorting
+        // Time Complexity = O(n^2)
+        int sel[] = { 7, 8, 23, 3, 1, 2, 55 };
+
+        for (int i = 0; i < sel.length - 1; i++) {
+            int small = i;
+            for (int j = i + 1; j < sel.length - 1; j++) {
+                if (sel[small] > sel[j]) {
+                    small = j;
+                }
+
+            }
+
+            int temp = sel[small];
+            sel[small] = sel[i];
+            sel[i] = temp;
+
+        }
+
+        displayArray(sel);
 
     }
 
