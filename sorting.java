860a83c0599960f29a96827df57febb0b482a929
Bhavneet Verma <bhavneetv295@gmail.com>
2025-10-05 13:24:28 +0530

    s

---
+++ sorting.java
@@ -0,0 +1,29 @@
+public class sorting {
+    static void displayArray(int arr[]) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // bubble sorting
+
+        int arr[] = { 4, 5, 2, 7, 8, 1 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        displayArray(arr);
+
+    }
+
+}
